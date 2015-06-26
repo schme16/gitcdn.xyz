43945f8eef1c60ff061af4f001c4504ec8e061bc
Shane Gadsby <schme16@gmail.com>
2015-06-27 01:04:58 +1000

    Added Caching for GitHub requests
    Signed-off-by: Shane Gadsby <schme16@gmail.com>

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ options = {
     }
 };
 
-
+cache = {}
 
 
 
@@ -49,16 +49,37 @@ app.get('/cdn/*', function (req, res) {
     }));
 });
 
+var lastCall = function (meta, body, req, res, cacheing) {
+    if (body && !cacheing) {
+        var newUrl = 'https://gitcdn' + (req.get('host').indexOf('min.gitcdn.xyz') !== -1 ? 'min' : '') + '-17ac.kxcdn.com/cdn/' + meta.user + '/' + meta.repo + '/' +  body.sha + '/' + meta.filePath;
+        cache[meta.t] = body;
+        res.setHeader('Content-Type', mime.lookup(newUrl));
+        res.redirect(301, newUrl)
+    }
+    else if (!!cacheing) {
+        cache[meta.t] = body;
+        console.log(3)
+    }
+    else {
+        res.sendStatus(500)
+    }
+}
 
 app.get('/repo/*', function (req, res) {
-    var t = req.path.substr(6)
-        raw = t.split('/'),
-        user = raw.shift(),
-        repo = raw.shift(),
-        branch = raw.shift(),
-        filePath = raw.join('/');
-
-    options.url = 'https://api.github.com/repos/' + user + '/' + repo + '/commits/master';
+    var meta = {};
+        meta.t = req.path.substr(6)
+        meta.raw = meta.t.split('/'),
+        meta.user = meta.raw.shift(),
+        meta.repo = meta.raw.shift(),
+        meta.branch = meta.raw.shift(),
+        meta.filePath = meta.raw.join('/'),
+        refreshCache = false;
+    options.url = 'https://api.github.com/repos/' + meta.user + '/' + meta.repo + '/commits/master';
+    if (cache[meta.t]) {
+        console.log(1)
+        refreshCache = true;
+        lastCall(meta, cache[meta.t], req, res);
+    }
     request.get(options, function (err, r, rawBody) {
         var body;
         if (rawBody) {
@@ -70,16 +91,10 @@ app.get('/repo/*', function (req, res) {
             catch (e) {
                 console.log(e)
             }
-
-            if (body) {
-                var newUrl = 'https://gitcdn' + (req.get('host').indexOf('min.gitcdn.xyz') !== -1 ? 'min' : '') + '-17ac.kxcdn.com/cdn/' + user + '/' + repo + '/' +  body.sha + '/' + filePath;
-                res.setHeader('Content-Type', mime.lookup(newUrl));
-                res.redirect(301, newUrl)
-                //res.send(newUrl)
-            }
-            else {
-                res.sendStatus(500)
-            }
+            
+            console.log(2)
+            lastCall(meta, body, req, res, refreshCache);
+            
         }
         else {
             res.sendStatus(500)
