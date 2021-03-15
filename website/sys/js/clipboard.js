be32f748248c531c3800274ff6898ab4b1d3da3a
Ved Nig <64083352+vednig@users.noreply.github.com>
2021-03-15 23:24:00 +0530

    Update clipboard.js

--- website/sys/js/clipboard.js
+++ website/sys/js/clipboard.js
@@ -9,7 +9,7 @@
         button.classList.add('btn-clipboard');
         button.classList.add('hidden');
         button.setAttribute('aria-label','Copy code to clipboard');
-        button.innerHTML = clipboardIcon + '<strong class="btn-clipboard__label waves-effect waves-light btn">Copy</strong>';
+        button.innerHTML = clipboardIcon + '<a class="btn-clipboard__label waves-effect waves-light btn">Copy</a>';
 
         inputElem.parentElement.appendChild(button);
 
